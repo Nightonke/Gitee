56fe580fbd2b3509e48896d51f6225c2d6297852
Nightonke <Nightonke@outlook.com>
2017-03-17 10:06:14 +0800

    1. Fixed issue #9(Support paste).

--- VHGithubNotifier/View/VHTextField.m
+++ VHGithubNotifier/View/VHTextField.m
@@ -18,6 +18,28 @@
     {
         [self.textFieldDelegate onTextFieldEnterButtonClicked:self];
     }
+    
+    if (([event modifierFlags] & NSDeviceIndependentModifierFlagsMask) == NSCommandKeyMask)
+    {
+        // The command key is the ONLY modifier key being pressed.
+        if ([[event charactersIgnoringModifiers] isEqualToString:@"x"])
+        {
+            return [NSApp sendAction:@selector(cut:) to:[[self window] firstResponder] from:self];
+        }
+        else if ([[event charactersIgnoringModifiers] isEqualToString:@"c"])
+        {
+            return [NSApp sendAction:@selector(copy:) to:[[self window] firstResponder] from:self];
+        }
+        else if ([[event charactersIgnoringModifiers] isEqualToString:@"v"])
+        {
+            return [NSApp sendAction:@selector(paste:) to:[[self window] firstResponder] from:self];
+        }
+        else if ([[event charactersIgnoringModifiers] isEqualToString:@"a"])
+        {
+            return [NSApp sendAction:@selector(selectAll:) to:[[self window] firstResponder] from:self];
+        }
+    }
+    
     return [super performKeyEquivalent:event];
 }
 
